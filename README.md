aff84717bc7f92c76c296bd8529f9c782fb7ac90
Komal Batra <KomalBatra@gmail.com>
2018-09-09 00:27:00 -0400

    Final version

--- README.md
+++ README.md
@@ -1,13 +1,67 @@
 # unit-4-game
-Homework # 4
-Creator: Komal Batra
 
-Started on:
-Competed on:
+### **Creator:** `Komal Batra`
+**Type of Project:** `JQuery Game`
+
+Started on: Sep 5th 2018
+Completed on: Sep 9th 2018
+- - -
+
+## Important Links
+
+* [Link to the game site](https://komalbatra.github.io/unit-4-game)
+
+* [Link to my repository](https://github.com/komalbatra/unit-4-game.git)
+
+- - -
+## Description of Application
+- - -
+
+Crystals Collector is a web-based game, where a user has to guess the value of the crystals in order to match a random number geneartes by the system. 
+
+The game is played as follows:
+
+1. Four crystals are displayed to the user as buttons on the page. 
+
+2. At the start of the game, a random number between 19-120 is displayed to the user. 
+
+3. Each crystal has a random number between 1-12 associated to it. 
+
+4. When the player clicks on a crystal, the system will add the value of the crystal to the user's total score.
+
+5. The game does not display the value associated with the crytal unless if the user clicks on it. 
+
+6. The player wins if their total score matches the random number from the beginning of the game.
+
+7. The player loses if their score goes above the random number.
+
+8. The game restarts whenever the player wins or loses.
+
+9. When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. 
+
+## JQuery File
+- - -
+`The game has 3 functions and 1 click event`
+
+Functions created/used in this game are:
+- **gameBegin:** Re-sets the game while leaving the wins/losses tally intact. Also generates the target random number and the hidden rndowm values of the 4 crystals.
+The random number shown at the start of the game should be between 19 - 120.
+Each crystal should have a random hidden value between 1 - 12.
+
+- **playGame:** Adds up the numbers associated to the crystal everytime a user clicks a crystal
+
+- **checkResult:** Checks if the crystal total equals the target random number
+
+- ** If greater,  then the system displayes " You Lost" to the user and incrments Losses score by 1
+If crystal counter matches the target randwom number, then the note "You Won" is displayed to the user and increments the score by one. 
+
+      The Click Event captures the users click and parses the number.
 
 
-Description of application
-
-List of Technologies Used
-
-Bugs
+## List of Technologies Used:
+- - - 
+- HTML 
+- CSS
+- Javascript
+- JQuery
+- Git
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,4 @@
-// Generte a random number between 19 and 120
-//Assign crystals with random numbers between 1 and 12
-//Numbers assigned to crystals should not be visible
-//When user clicks on a crystal, the number should display in a box below.
-//user will continue to click on teh crystal. 
-//The numbers in the box below should add up.
-//if the number in the box below, matches the number in the random box, then:
-//display "You Won!" and increments Wins score by 1
-//otherwise display "You Lost!" and increment Loses score by 1
-//when user loses or wins, a new game starts. 
-//New random number is generated
-//Crystals get new random numbers
-//Counter for wins and losses 
-
+// Javascript file for HOMEWORK #4 Crystals Collector Game.
 
 $(document).ready(function(){
     //Declaring Variables   
@@ -28,65 +15,64 @@ $(document).ready(function(){
     $("#loss").text(losses);
 
     //Function that initializes the next game and generates random numbers
-function gameBegin () {
-    
-    var targetNumber = Math.floor(Math.random() *(120-19+1)) + 19; 
-    $("#guess-number").text(targetNumber);
-   
-    var randomCrysValue1 = Math.floor(Math.random() * 12) + 1;
-    $("#crys-img1").attr ("data-crystalvalue", randomCrysValue1);    
+    function gameBegin () {
+        
+        crysCounterTotal = 0;
+        
+        // Target Randowm number 
+        targetNumber = Math.floor(Math.random() *(120-19+1)) + 19; 
+        $("#guess-number").text(targetNumber);
+                
+        // Random number generation for each of the 4 crystals
+        var randomCrysValue1 = Math.floor(Math.random() * 12) + 1;
+        $("#crys-img1").attr ("data-crystalvalue", randomCrysValue1);    
+               
+        var randomCrysValue2 = Math.floor(Math.random() * 12) + 1;
+        $("#crys-img2").attr ("data-crystalvalue", randomCrysValue2);
+       
+        var randomCrysValue3 = Math.floor(Math.random() * 12) + 1;
+        $("#crys-img3").attr ("data-crystalvalue", randomCrysValue3);
+        
+        var randomCrysValue4 = Math.floor(Math.random() * 12) + 1;
+        $("#crys-img4").attr ("data-crystalvalue", randomCrysValue4);
+        }
 
-    var randomCrysValue2 = Math.floor(Math.random() * 12) + 1;
-    $("#crys-img2").attr ("data-crystalvalue", randomCrysValue2);
 
-    var randomCrysValue3 = Math.floor(Math.random() * 12) + 1;
-    $("#crys-img3").attr ("data-crystalvalue", randomCrysValue3);
+        // Function that adds crystal values
+    function playGame (crystalValue) {
+        crysCounterTotal += crystalValue;
+        $("#score").text(crysCounterTotal);
+        }
 
-    var randomCrysValue4 = Math.floor(Math.random() * 12) + 1;
-    $("#crys-img4").attr ("data-crystalvalue", randomCrysValue4);
+    //function checks if win or loss
+    function checkResult() {
+        if (crysCounterTotal > targetNumber)
+        {
+            $("#announce").text("You Lost"); 
+            losses ++;
+            $("#loss").text(losses);
+            gameBegin();  // initialize game
+        }
+        else if (crysCounterTotal === targetNumber)
+        {   
+            $("#announce").text("You Won"); 
+            wins ++;
+            $("#wins").text(wins);    
+            gameBegin(); //initialize game
+        } 
+        else if (crysCounterTotal < targetNumber)
+        {
+            $("#announce").text(""); 
+        }
     }
-
-
-function playGame (crystalValue) {
-    crysCounterTotal += crystalValue;
-    $("#score").text(crysCounterTotal);
-    console.log ("Counter Total" + "" + crysCounterTotal);
-    }
-
-//check if win or loss
-function checkResult() {
-   if (crysCounterTotal === targetNumber){   
-       console.log("crysCounterTotal is" + crysCounterTotal);
-       console.log("targetNumber is" + targetNumber);
-        wins ++;
-        console.log("wins" + wins);
-        $("#wins").text(wins);    
-        gameBegin(); //initialize game
-    } 
-
-    if (crysCounterTotal >= targetNumber){
-       console.log("Loss crysCounterTotal is" + crysCounterTotal);
-       console.log("Loss targetNumber is" + targetNumber);
-        losses ++;
-        console.log("Losses" + losses);
-        $("#loss").text(losses);
-        gameBegin();  // initialize game
-    }
-    if (crysCounterTotal < targetNumber){
-        console.log("Loss crysCounterTotal is" + crysCounterTotal);
-       console.log("Loss targetNumber is" + targetNumber);
-    }
-}
-
-
-$(".crystal-image").on("click", function() {
-        var crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue);
-        playGame(crystalValue);    
-        checkResult();       
-    });
-
-gameBegin();
-
+    // On Click event   
+    $(".crystal-image").on("click", function() 
+        {
+            var crystalValue = ($(this).attr("data-crystalvalue"));
+            crystalValue = parseInt(crystalValue);
+            playGame(crystalValue);    
+            checkResult();
+           });
+    gameBegin();
 });
 
